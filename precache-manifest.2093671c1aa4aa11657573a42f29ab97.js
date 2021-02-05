1e168060bf65cf27bbf507acf911b5a0a2e41952
Leheng620 <llh238704688@gmail.com>
2021-02-05 18:43:53 -0500

    published

---
+++ precache-manifest.2093671c1aa4aa11657573a42f29ab97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cba0abc987453534c24f243501747d5",
+    "url": "/todo-list-firebase/index.html"
+  },
+  {
+    "revision": "bba3b407781e6828bd84",
+    "url": "/todo-list-firebase/static/css/main.4a467e57.chunk.css"
+  },
+  {
+    "revision": "e82521cf62505d5535b1",
+    "url": "/todo-list-firebase/static/js/2.a41b8f65.chunk.js"
+  },
+  {
+    "revision": "bba3b407781e6828bd84",
+    "url": "/todo-list-firebase/static/js/main.c15b7c28.chunk.js"
+  },
+  {
+    "revision": "d36bdaa9c3032b4ca539",
+    "url": "/todo-list-firebase/static/js/runtime-main.cfffc0f5.js"
+  }
+]);
